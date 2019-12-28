c940eb06304bd1fa18727de76533df6d95c101db
rjbma <ricardo.araujo@inovaprime.com>
2019-12-28 19:19:55 +0000

    add an example with a (crappy) editable table with (even crappier) key navigation

--- src/MainDemo.elm
+++ src/MainDemo.elm
@@ -1,5 +1,6 @@
 module MainDemo exposing (main)
 
+import Array exposing (Array)
 import Browser
 import Html exposing (Html)
 import Html.Attributes exposing (src)
@@ -16,6 +17,7 @@ type alias Character =
     , power : Int
     , imageUrl : String
     , hatType : HatType
+    , values : Array Int
     }
 
 
@@ -27,17 +29,22 @@ type HatType
     | Regular
 
 
-rows : List Character
-rows =
-    [ { fullName = "McGucket", tagLine = "beard-aid", power = 10000, hatType = Gandalf, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/f/f5/S1e14_if_you_survive.png/revision/latest?cb=20130305201426" }
-    , { fullName = "Soos", tagLine = "a vida é um regabofe", power = 50, hatType = Sports, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/b/bb/S1e13_whole_busload_of_em.png/revision/latest/top-crop/width/200/height/150?cb=20151221063833" }
-    , { fullName = "Mable", tagLine = "let's go girls", power = 1000, hatType = None, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/b/b2/S1e3_mabel_new_wax_figure.png/revision/latest/top-crop/width/200/height/150?cb=20160204031402" }
-    , { fullName = "Dipper", tagLine = "let's investigate", power = 1000, hatType = Sports, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/c/cb/S1e16_dipper_will_take_room.png/revision/latest/top-crop/width/200/height/150?cb=20160119145706" }
-    , { fullName = "Toby Carbonato", tagLine = "it's me", power = 999999999, hatType = Regular, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/9/9e/S1e3_toby_determined_hands_in_air.png/revision/latest?cb=20120705020811" }
-    , { fullName = "Stan Pines", tagLine = "não é pequeno", power = 20000, hatType = Wtf, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/9/92/S1e16_something_about_you.png/revision/latest/top-crop/width/200/height/150?cb=20130530141339" }
-    , { fullName = "Pacifica", tagLine = "who?", power = 10, hatType = None, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/f/f7/S1e7_Pacifica_Northwest.png/revision/latest/top-crop/width/200/height/150?cb=20151116020530" }
-    , { fullName = "Bill", tagLine = "ora ora ora ora ora ora", power = 20000, hatType = Regular, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/e/ea/S1e19_bill_snap.png/revision/latest/top-crop/width/200/height/150?cb=20130905173319" }
-    , { fullName = "Waddles", tagLine = "oinc oinc", power = 2, hatType = None, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/0/09/S1e18_Waddles_stare.png/revision/latest/scale-to-width-down/350?cb=20160119145820" }
+initialValues : Array Int
+initialValues =
+    Array.repeat numberOfDays 0
+
+
+initialRows : List Character
+initialRows =
+    [ { fullName = "McGucket", tagLine = "beard-aid", power = 10000, hatType = Gandalf, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/f/f5/S1e14_if_you_survive.png/revision/latest?cb=20130305201426", values = initialValues }
+    , { fullName = "Soos", tagLine = "a vida é um regabofe", power = 50, hatType = Sports, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/b/bb/S1e13_whole_busload_of_em.png/revision/latest/top-crop/width/200/height/150?cb=20151221063833", values = initialValues }
+    , { fullName = "Mable", tagLine = "let's go girls", power = 1000, hatType = None, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/b/b2/S1e3_mabel_new_wax_figure.png/revision/latest/top-crop/width/200/height/150?cb=20160204031402", values = initialValues }
+    , { fullName = "Dipper", tagLine = "let's investigate", power = 1000, hatType = Sports, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/c/cb/S1e16_dipper_will_take_room.png/revision/latest/top-crop/width/200/height/150?cb=20160119145706", values = initialValues }
+    , { fullName = "Toby Carbonato", tagLine = "it's me", power = 999999999, hatType = Regular, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/9/9e/S1e3_toby_determined_hands_in_air.png/revision/latest?cb=20120705020811", values = initialValues }
+    , { fullName = "Stan Pines", tagLine = "não é pequeno", power = 20000, hatType = Wtf, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/9/92/S1e16_something_about_you.png/revision/latest/top-crop/width/200/height/150?cb=20130530141339", values = initialValues }
+    , { fullName = "Pacifica", tagLine = "who?", power = 10, hatType = None, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/f/f7/S1e7_Pacifica_Northwest.png/revision/latest/top-crop/width/200/height/150?cb=20151116020530", values = initialValues }
+    , { fullName = "Bill", tagLine = "ora ora ora ora ora ora", power = 20000, hatType = Regular, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/e/ea/S1e19_bill_snap.png/revision/latest/top-crop/width/200/height/150?cb=20130905173319", values = initialValues }
+    , { fullName = "Waddles", tagLine = "oinc oinc", power = 2, hatType = None, imageUrl = "https://vignette.wikia.nocookie.net/gravityfalls/images/0/09/S1e18_Waddles_stare.png/revision/latest/scale-to-width-down/350?cb=20160119145820", values = initialValues }
     ]
 
 
@@ -84,24 +91,37 @@ type Example2ViewMode
     | Example2FlexCards
 
 
+numberOfDays : Int
+numberOfDays =
+    5
+
+
 type alias Model =
-    { listViewState : ListView.State
+    { rows : List Character
+
+    -- example #1
+    , example1State : ListView.State
 
     -- example #2
     , example2State : ListView.State
     , example2ViewMode : Example2ViewMode
+
+    -- example #3
+    , example3State : ListView.State
     }
 
 
 initialModel : Model
 initialModel =
-    { listViewState =
+    { rows = initialRows
+    , example1State =
         ListView.makeState
             |> ListView.updateState.withRowsPerPage 4
     , example2State =
         ListView.makeState
-            |> ListView.updateState.withRowsPerPage 4
+            |> ListView.updateState.withRowsPerPage 6
     , example2ViewMode = Example2HtmlTable
+    , example3State = ListView.makeState
     }
 
 
@@ -109,19 +129,41 @@ type Msg
     = Example1ListViewMsg ListViewMsg
     | Example2ListViewMsg ListViewMsg
     | SwitchExample2ViewMode Example2ViewMode
+    | Ex3UpdateCell Character Int String
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         Example1ListViewMsg listViewMsg ->
-            { model | listViewState = ListView.Viewers.update rows listViewMsg model.listViewState }
+            { model | example1State = ListView.Viewers.update model.rows listViewMsg model.example1State }
 
         Example2ListViewMsg listViewMsg ->
-            { model | example2State = ListView.Viewers.update rows listViewMsg model.example2State }
+            { model | example2State = ListView.Viewers.update model.rows listViewMsg model.example2State }
 
         SwitchExample2ViewMode newViewMode ->
             { model | example2ViewMode = newViewMode }
+
+        Ex3UpdateCell character dayNumber strValue ->
+            let
+                intValue =
+                    String.toInt strValue |> Maybe.withDefault 0
+
+                updateValuesForCharacter =
+                    \c -> { c | values = Array.set dayNumber intValue c.values }
+
+                newRows =
+                    List.map
+                        (\c ->
+                            if c == character then
+                                updateValuesForCharacter c
+
+                            else
+                                c
+                        )
+                        model.rows
+            in
+            { model | rows = newRows }
 
 
 view : Model -> Html Msg
@@ -129,18 +171,22 @@ view model =
     Html.div [ class_ "container" ]
         [ Html.h1 [] [ Html.text "elm-listview" ]
         , Html.p [] [ Markdown.toHtml [] "An **Elm** library for viewing data in a `List`." ]
+        , viewExample1 model
+        , viewExample2 model
+        , viewExample3 model
+        ]
 
-        -- example #1
-        , Html.h2 [] [ Html.text "Simple table" ]
+
+viewExample1 : Model -> Html Msg
+viewExample1 model =
+    Html.article [ class_ "example1" ]
+        [ Html.h2 [] [ Html.text "Simple table" ]
         , Html.p [] [ Markdown.toHtml [] """Render data in a HTML table.
 
 Cells can contain simple data (`String`, `Int`, `Float`), but can also contain any HTML element.""" ]
         , Html.div [ class_ "gravityTable" ]
-            [ ListView.Viewers.viewAsHtmlTable Example1ListViewMsg listViewConfig model.listViewState rows
+            [ ListView.Viewers.viewAsHtmlTable Example1ListViewMsg listViewConfig model.example1State model.rows
             ]
-
-        -- example #2
-        , viewExample2 model
         ]
 
 
@@ -159,21 +205,61 @@ viewExample2 model =
                 Example2HtmlTable ->
                     Html.div [ class_ "gravityTable" ]
                         [ Markdown.toHtml [] "This view uses a simple HTML table with `layout: auto`"
-                        , ListView.Viewers.viewAsHtmlTable Example2ListViewMsg listViewConfig model.example2State rows
+                        , ListView.Viewers.viewAsHtmlTable Example2ListViewMsg listViewConfig model.example2State model.rows
                         ]
 
                 Example2CssGrid ->
                     Html.div [ class_ "gravityGrid" ]
                         [ Markdown.toHtml [] "This view uses `CSS Grid` to try and mimic the HTML table from the first view. It actually uses a grid for each row because of the padding between rows.\n\nThis table is responsive, it's rendered differently on smaller screens (there's probably a better way to achieve that)."
-                        , ListView.Viewers.viewAsCssGrid Example2ListViewMsg listViewConfig model.example2State rows
+                        , ListView.Viewers.viewAsCssGrid Example2ListViewMsg listViewConfig model.example2State model.rows
                         ]
 
                 Example2FlexCards ->
                     Html.div [ class_ "gravityDeck" ]
                         [ Markdown.toHtml [] "This view uses `Flexbox` to display each row as a card. \n\nNote this is actually the same viewer as for `CSS Grid`, only the layout is changed using CSS."
-                        , ListView.Viewers.viewAsCssGrid Example2ListViewMsg listViewConfig model.example2State rows
+                        , ListView.Viewers.viewAsCssGrid Example2ListViewMsg listViewConfig model.example2State model.rows
                         ]
             ]
+        ]
+
+
+getIntFromArray : Int -> Array Int -> Int
+getIntFromArray i array =
+    Array.get i array
+        |> Maybe.withDefault 0
+
+
+viewExample3 : Model -> Html Msg
+viewExample3 model =
+    let
+        makeDayColumn =
+            \i ->
+                ListView.makeColumn.html ("Day " ++ String.fromInt i)
+                    (\character ->
+                        Html.input
+                            [ class_ "example3-dayInput"
+                            , Html.Attributes.value
+                                (getIntFromArray i character.values
+                                    |> String.fromInt
+                                )
+                            , Html.Events.onInput (Ex3UpdateCell character i)
+                            ]
+                            []
+                    )
+
+        cols =
+            List.range 1 numberOfDays
+                |> List.map makeDayColumn
+
+        lvConfig =
+            ListView.makeConfig
+                |> ListView.withColumn (ListView.makeColumn.string "Name" .fullName)
+                |> ListView.withColumns cols
+    in
+    Html.article [ class_ "example2" ]
+        [ Html.h2 [] [ Html.text "Editable table" ]
+        , Html.div [ class_ "gravityTable" ]
+            [ ListView.Viewers.viewAsHtmlTable Example1ListViewMsg lvConfig model.example1State model.rows ]
         ]
 
 
