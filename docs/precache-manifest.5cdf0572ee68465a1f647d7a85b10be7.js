06fb0ad07c27525948eddbe301a4c3ad81365007
rjbma <ricardo.araujo@inovaprime.com>
2020-01-01 16:27:59 +0000

    added docs for GiHub Pages; moved images to correct location

---
+++ docs/precache-manifest.5cdf0572ee68465a1f647d7a85b10be7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "a88ef790081d1dd284fe0a5a7604d9cd",
+    "url": "/images/bill.jpg"
+  },
+  {
+    "revision": "1dfd17c529bbbb607e1f5df5bef5ffa4",
+    "url": "/images/dipper.jpg"
+  },
+  {
+    "revision": "1e47719b1c2f8d67b7d34db0f93e8309",
+    "url": "/images/mable.jpg"
+  },
+  {
+    "revision": "709265284711148c2301961f6daad474",
+    "url": "/images/old-man-mcgucket.jpg"
+  },
+  {
+    "revision": "1a0263a29f791292168aab4cd17dcf98",
+    "url": "/images/pacifica.jpg"
+  },
+  {
+    "revision": "2042244c7e200a8f41c7d951f0187a0f",
+    "url": "/images/soos.png"
+  },
+  {
+    "revision": "74a4387745bf6f51fb1b1abeb855a1db",
+    "url": "/images/stan.jpg"
+  },
+  {
+    "revision": "b6ff9e1f9f126b834ebabb340930af74",
+    "url": "/images/toby.jpg"
+  },
+  {
+    "revision": "4c890a7b304d482881ae5866d1386e55",
+    "url": "/images/waddles.jpg"
+  },
+  {
+    "revision": "71f48fdea5ab28af20e8c2a1c45e6f33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "2223857aa56d4b0ac4da",
+    "url": "/static/css/main.7b078b2e.chunk.css"
+  },
+  {
+    "revision": "2223857aa56d4b0ac4da",
+    "url": "/static/js/main.2223857a.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "aec9f19d02e672128858",
+    "url": "/static/js/vendors~main.aec9f19d.chunk.js"
+  }
+]);
