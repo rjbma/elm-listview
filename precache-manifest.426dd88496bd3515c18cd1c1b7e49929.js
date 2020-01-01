92153f0cbc422df265023ab38806491488dbf8dd
rjbma <ricardo.araujo@inovaprime.com>
2020-01-01 17:08:28 +0000

    Updates

---
+++ precache-manifest.426dd88496bd3515c18cd1c1b7e49929.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/elm-listview/favicon.ico"
+  },
+  {
+    "revision": "a88ef790081d1dd284fe0a5a7604d9cd",
+    "url": "/elm-listview/images/bill.jpg"
+  },
+  {
+    "revision": "1dfd17c529bbbb607e1f5df5bef5ffa4",
+    "url": "/elm-listview/images/dipper.jpg"
+  },
+  {
+    "revision": "1e47719b1c2f8d67b7d34db0f93e8309",
+    "url": "/elm-listview/images/mable.jpg"
+  },
+  {
+    "revision": "709265284711148c2301961f6daad474",
+    "url": "/elm-listview/images/old-man-mcgucket.jpg"
+  },
+  {
+    "revision": "1a0263a29f791292168aab4cd17dcf98",
+    "url": "/elm-listview/images/pacifica.jpg"
+  },
+  {
+    "revision": "2042244c7e200a8f41c7d951f0187a0f",
+    "url": "/elm-listview/images/soos.png"
+  },
+  {
+    "revision": "74a4387745bf6f51fb1b1abeb855a1db",
+    "url": "/elm-listview/images/stan.jpg"
+  },
+  {
+    "revision": "b6ff9e1f9f126b834ebabb340930af74",
+    "url": "/elm-listview/images/toby.jpg"
+  },
+  {
+    "revision": "4c890a7b304d482881ae5866d1386e55",
+    "url": "/elm-listview/images/waddles.jpg"
+  },
+  {
+    "revision": "fe290052a96e1d5d7a715d0922486c51",
+    "url": "/elm-listview/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/elm-listview/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/elm-listview/manifest.json"
+  },
+  {
+    "revision": "2223857aa56d4b0ac4da",
+    "url": "/elm-listview/static/css/main.7b078b2e.chunk.css"
+  },
+  {
+    "revision": "2223857aa56d4b0ac4da",
+    "url": "/elm-listview/static/js/main.2223857a.chunk.js"
+  },
+  {
+    "revision": "c8d4f771063f6a727f17",
+    "url": "/elm-listview/static/js/runtime~main.c8d4f771.js"
+  },
+  {
+    "revision": "aec9f19d02e672128858",
+    "url": "/elm-listview/static/js/vendors~main.aec9f19d.chunk.js"
+  }
+]);
